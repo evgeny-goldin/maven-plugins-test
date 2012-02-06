220b240684f9bd8cf50e70cef9472f6cbfb59dff
Evgeny Goldin <evgenyg@gmail.com>
2012-02-07 01:36:20 +0200

    "maven-ivy-plugin" - Ant dependency test added

---
+++ maven-ivy-plugin/src/main/java/MyAntTask.java
@@ -0,0 +1,6 @@
+/**
+ * Test interface, Ant is added to "compile" classpath with "maven-ivy-plugin" at runtime.
+ */
+public abstract class MyAntTask extends org.apache.tools.ant.Task
+{
+}
--- maven-ivy-plugin/src/main/java/MyLog4jAppender.java
+++ maven-ivy-plugin/src/main/java/MyLog4jAppender.java
@@ -1,5 +1,6 @@
 /**
  * Test interface, Log4j is added to "compile" classpath with "maven-ivy-plugin" at runtime.
  */
-interface MyLog4jAppender extends org.apache.log4j.Appender {
+interface MyLog4jAppender extends org.apache.log4j.Appender
+{
 }
