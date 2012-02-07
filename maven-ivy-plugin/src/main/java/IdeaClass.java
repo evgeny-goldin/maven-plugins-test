8118754569207935914007fd2d8bad4cd283aff8
Evgeny Goldin <evgenyg@gmail.com>
2012-02-07 04:28:30 +0200

    "maven-ivy-plugin" - more tests added.

---
+++ maven-ivy-plugin/src/main/java/IdeaClass.java
@@ -0,0 +1,7 @@
+/**
+ * Test class, "intellij-core.jar" is added to "compile" classpath with "maven-ivy-plugin" at runtime.
+ */
+public class IdeaClass
+{
+    private com.intellij.openapi.util.text.StringUtil util;
+}
--- maven-ivy-plugin/src/main/java/MyAntTask.java
+++ maven-ivy-plugin/src/main/java/MyAntTask.java
@@ -1,5 +1,5 @@
 /**
- * Test interface, Ant is added to "compile" classpath with "maven-ivy-plugin" at runtime.
+ * Test class, Ant is added to "compile" classpath with "maven-ivy-plugin" at runtime.
  */
 public abstract class MyAntTask extends org.apache.tools.ant.Task
 {
