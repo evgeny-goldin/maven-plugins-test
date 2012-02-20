a633116a149c699e924503a7729dd29cb7ed0287
Evgeny Goldin <evgenyg@gmail.com>
2012-02-20 22:54:17 +0200

    Kotlin plugin tests - running unit tests.

--- kotlin-maven-plugin/src/test/module.kt
+++ kotlin-maven-plugin/src/test/module.kt
@@ -2,8 +2,6 @@ import kotlin.modules.*
 
 fun project() {
     module("testlib") {
-        // TODO how to refer to the dir of the module?
-        // classpath += "testlib/lib/junit-4.9.jar"
         addSourceFiles("testlib")
     }
 }
