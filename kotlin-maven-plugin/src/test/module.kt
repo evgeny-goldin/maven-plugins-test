e13a51bb0ae80b1acc6be966d295f91408cbea71
Evgeny Goldin <evgenyg@gmail.com>
2012-02-20 17:47:01 +0200

    Ivy plugin tests - "testlib" move to "src/test/testlib/", POM update.

---
+++ kotlin-maven-plugin/src/test/module.kt
@@ -0,0 +1,9 @@
+import kotlin.modules.*
+
+fun project() {
+    module("testlib") {
+        // TODO how to refer to the dir of the module?
+        // classpath += "testlib/lib/junit-4.9.jar"
+        addSourceFiles("testlib")
+    }
+}
---
+++ kotlin-maven-plugin/src/test/testlib/dom/DomBuilderTest.kt
@@ -0,0 +1,43 @@
+package test.dom
+
+import std.*
+import std.dom.*
+import std.util.*
+import stdhack.test.*
+import org.w3c.dom.*
+
+class DomBuilderTest() : TestSupport() {
+
+    fun testBuildDocument() {
+        var doc = createDocument()
+
+        doc.addElement("foo") {
+            id = "id1"
+            style = "bold"
+            cssClass = "bar"
+            addElement("child") {
+                cssClass = "another"
+                addElement("grandChild") {
+                    cssClass = "tiny"
+                    addText("Hello World!")
+                // TODO support neater syntax sugar for adding text?
+                // += "Hello World!"
+                }
+            }
+        }
+        println("builder document: ${doc.toXmlString()}")
+
+
+        val grandChild = doc.elementsByTagName("grandChild").first
+        if (grandChild != null) {
+            println("got element ${grandChild.toXmlString()} with text '${grandChild.text}`")
+            assertEquals("Hello World!", grandChild.text)
+        } else {
+            fail("Not an Element $grandChild")
+        }
+        val children = doc.rootElement.children()
+        val xml = children.toXmlString()
+        println("root element has children: ${xml}")
+        assertEquals(1, children.size())
+    }
+}
